9cf2ca963d98f73d6167390fd22218d30c9c2cdb
nguyentranlht <88877923+nguyentranlht@users.noreply.github.com>
2024-05-05 02:09:16 +0700

    upload fire base

--- flutter_hotel_booking_ui/lib/main.dart
+++ flutter_hotel_booking_ui/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_hotel_booking_ui/firebase_options.dart';
 import 'package:flutter_hotel_booking_ui/utils/themes.dart';
 import 'package:flutter_hotel_booking_ui/providers/theme_provider.dart';
 import 'package:flutter_hotel_booking_ui/motel_app.dart';
@@ -7,6 +9,8 @@ import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,
+  );
   await SystemChrome.setPreferredOrientations(
           [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
       .then((_) => runApp(_setAllProviders()));
