cbf4c82d0e27d6bcbf084bc30438e56c9fb8ab7b
nguyentranlht <88877923+nguyentranlht@users.noreply.github.com>
2024-08-07 17:49:09 +0700

    chatbotai

--- flutter_hotel_booking_ui/lib/modules/profile/hepl_center_screen.dart
+++ flutter_hotel_booking_ui/lib/modules/profile/hepl_center_screen.dart
@@ -1,14 +1,6 @@
+import 'package:dialog_flowtter/dialog_flowtter.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hotel_booking_ui/language/appLocalizations.dart';
-import 'package:flutter_hotel_booking_ui/routes/route_names.dart';
-import 'package:flutter_hotel_booking_ui/utils/text_styles.dart';
-import 'package:flutter_hotel_booking_ui/utils/themes.dart';
-import 'package:flutter_hotel_booking_ui/widgets/common_appbar_view.dart';
-import 'package:flutter_hotel_booking_ui/widgets/common_card.dart';
-import 'package:flutter_hotel_booking_ui/widgets/common_search_bar.dart';
-import 'package:flutter_hotel_booking_ui/widgets/remove_focuse.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import '../../models/setting_list_data.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class HeplCenterScreen extends StatefulWidget {
   @override
@@ -16,121 +8,196 @@ class HeplCenterScreen extends StatefulWidget {
 }
 
 class _HeplCenterScreenState extends State<HeplCenterScreen> {
+  late DialogFlowtter dialogFlowtter;
+  final TextEditingController messageController = TextEditingController();
+
+  List<Map<String, dynamic>> messages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    DialogFlowtter.fromFile().then((instance) => dialogFlowtter = instance);
+  }
+
   @override
   Widget build(BuildContext context) {
-    List<SettingsListData> helpSearchList = SettingsListData.helpSearchList;
+    var themeValue = MediaQuery.of(context).platformBrightness;
     return Scaffold(
-      body: RemoveFocuse(
-        onClick: () {
-          FocusScope.of(context).requestFocus(FocusNode());
-        },
+      backgroundColor: themeValue == Brightness.dark
+          ? HexColor('#262626')
+          : HexColor('#FFFFFF'),
+      appBar: AppBar(
+        backgroundColor: themeValue == Brightness.dark
+            ? HexColor('#3C3A3A')
+            : HexColor('#BFBFBF'),
+        title: Text(
+          'Flutter Bot',
+          style: TextStyle(
+              color:
+                  themeValue == Brightness.dark ? Colors.white : Colors.black),
+        ),
+      ),
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
+          children: [
+            Expanded(child: Body(messages: messages)),
             Container(
-              color: Theme.of(context).primaryColor,
-              child: Padding(
-                padding: EdgeInsets.only(bottom: 24),
-                child: appBar(),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 5,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: messageController,
+                      style: TextStyle(
+                          color: themeValue == Brightness.dark
+                              ? Colors.white
+                              : Colors.black,
+                          fontFamily: 'Poppins'),
+                      decoration: new InputDecoration(
+                        enabledBorder: new OutlineInputBorder(
+                            borderSide: new BorderSide(
+                                color: themeValue == Brightness.dark
+                                    ? Colors.white
+                                    : Colors.black),
+                            borderRadius: BorderRadius.circular(15)),
+                        hintStyle: TextStyle(
+                          color: themeValue == Brightness.dark
+                              ? Colors.white54
+                              : Colors.black54,
+                          fontSize: 15,
+                          fontStyle: FontStyle.italic,
+                        ),
+                        labelStyle: TextStyle(
+                            color: themeValue == Brightness.dark
+                                ? Colors.white
+                                : Colors.black),
+                        hintText: 'Send a message',
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    color: themeValue == Brightness.dark
+                        ? Colors.white
+                        : Colors.black,
+                    icon: Icon(Icons.send),
+                    onPressed: () {
+                      sendMessage(messageController.text);
+                      messageController.clear();
+                    },
+                  ),
+                ],
               ),
             ),
-            Expanded(
-              child: ListView.builder(
-                padding: EdgeInsets.only(
-                    bottom: MediaQuery.of(context).padding.bottom),
-                itemCount: helpSearchList.length,
-                itemBuilder: (context, index) {
-                  return InkWell(
-                    onTap: helpSearchList[index].subTxt != ""
-                        ? () {
-                            NavigationServices(context).gotoHowDoScreen();
-                          }
-                        : null,
-                    child: Column(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8, right: 16),
-                          child: Row(
-                            children: <Widget>[
-                              Expanded(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(16.0),
-                                  child: Text(
-                                    helpSearchList[index].titleTxt != ""
-                                        ? AppLocalizations(context)
-                                            .of(helpSearchList[index].titleTxt)
-                                        : AppLocalizations(context)
-                                            .of(helpSearchList[index].subTxt),
-                                    style: TextStyles(context)
-                                        .getRegularStyle()
-                                        .copyWith(
-                                            fontWeight: helpSearchList[index]
-                                                        .titleTxt !=
-                                                    ""
-                                                ? FontWeight.bold
-                                                : FontWeight.normal,
-                                            fontSize: helpSearchList[index]
-                                                        .titleTxt !=
-                                                    ""
-                                                ? 18
-                                                : 14),
-                                  ),
-                                ),
-                              ),
-                              helpSearchList[index].subTxt != ""
-                                  ? Padding(
-                                      padding: const EdgeInsets.all(16),
-                                      child: Icon(Icons.keyboard_arrow_right,
-                                          color: Theme.of(context)
-                                              .disabledColor
-                                              .withOpacity(0.3)),
-                                    )
-                                  : SizedBox()
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 16, right: 16),
-                          child: Divider(
-                            height: 1,
-                          ),
-                        )
-                      ],
-                    ),
-                  );
-                },
-              ),
-            )
           ],
         ),
       ),
     );
   }
 
-  Widget appBar() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        CommonAppbarView(
-          onBackClick: () {
-            Navigator.pop(context);
-          },
-          iconData: Icons.arrow_back,
-          titleText: AppLocalizations(context).of("how_can_help_you"),
-        ),
-        Padding(
-            padding: const EdgeInsets.only(left: 24, right: 24, top: 16),
-            child: CommonCard(
-              color: AppTheme.backgroundColor,
-              radius: 36,
-              child: CommonSearchBar(
-                iconData: FontAwesomeIcons.search,
-                text: AppLocalizations(context).of("search_help_artical"),
+  void sendMessage(String text) async {
+    if (text.isEmpty) return;
+    setState(() {
+      addMessage(
+        Message(text: DialogText(text: [text])),
+        true,
+      );
+    });
+
+    DetectIntentResponse response = await dialogFlowtter.detectIntent(
+      queryInput: QueryInput(text: TextInput(text: text)),
+    );
+
+    if (response.message == null) return;
+    setState(() {
+      addMessage(response.message!);
+    });
+  }
+
+  void addMessage(Message message, [bool isUserMessage = false]) {
+    messages.add({
+      'message': message,
+      'isUserMessage': isUserMessage,
+    });
+  }
+
+  @override
+  void dispose() {
+    dialogFlowtter.dispose();
+    super.dispose();
+  }
+}
+class Body extends StatelessWidget {
+  final List<Map<String, dynamic>> messages;
+
+  const Body({
+    Key? key,
+    this.messages = const [],
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemBuilder: (context, i) {
+        var obj = messages[messages.length - 1 - i];
+        Message message = obj['message'];
+        bool isUserMessage = obj['isUserMessage'] ?? false;
+        return Row(
+          mainAxisAlignment:
+              isUserMessage ? MainAxisAlignment.end : MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            _MessageContainer(
+              message: message,
+              isUserMessage: isUserMessage,
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (_, i) => Container(height: 10),
+      itemCount: messages.length,
+      reverse: true,
+      padding: const EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 20,
+      ),
+    );
+  }
+}
+
+class _MessageContainer extends StatelessWidget {
+  final Message message;
+  final bool isUserMessage;
+
+  const _MessageContainer({
+    Key? key,
+    required this.message,
+    this.isUserMessage = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(maxWidth: 250),
+      child: LayoutBuilder(
+        builder: (context, constrains) {
+          return Container(
+            decoration: BoxDecoration(
+              color: isUserMessage ? Colors.blue : Colors.grey[800],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: const EdgeInsets.all(10),
+            child: Text(
+              message.text?.text?[0] ?? '',
+              style: TextStyle(
+                color: Colors.white,
               ),
-            )),
-      ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
